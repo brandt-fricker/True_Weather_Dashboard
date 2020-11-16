6be87e215413284bddeece987810436a819e5304
brandt fricker <brandtfricker@brandts-MacBook-Pro.local>
2020-11-16 10:03:13 -0700

    search results

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 var apikey = "2d3fc2e3e0711ca594c4d9d5c7d37e7f"
 
-function userCity(){
+function userCity() {
 
     // Get the entry from the input field.
     let citySearch = document.querySelector("#city").value;
-    
+
     // Make sure the entry is "valid"
     // Trim off white-space
     // Check length
     // If too short, warn user and return
-
-
-
-
     // Store to localstorage
+    if (citySearch !== "" || citySearch !== undefined) {
+        citySearch.trim();
+        localStorage.setItem("UserCitySelection", JSON.stringify(citySearch));
+        var ul = document.getElementById("myUL");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(citySearch));
+        ul.appendChild(li);
+
+    } else {
+        alert("Please enter a valid city")
+    }
+
+
+
+
 
 
     // Update the recent searches area
@@ -27,5 +38,5 @@ function userCity(){
 
     // Display the (format) results in the output div
 
-    
+
 }
