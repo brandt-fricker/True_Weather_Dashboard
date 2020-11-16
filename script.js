6e1a8306dac7ecd461a115749541b1a58fc53688
brandt fricker <brandtfricker@brandts-MacBook-Pro.local>
2020-11-15 22:04:36 -0700

    work in progress

---
+++ script.js
@@ -0,0 +1,31 @@
+var apikey = "2d3fc2e3e0711ca594c4d9d5c7d37e7f"
+
+function userCity(){
+
+    // Get the entry from the input field.
+    let citySearch = document.querySelector("#city").value;
+    
+    // Make sure the entry is "valid"
+    // Trim off white-space
+    // Check length
+    // If too short, warn user and return
+
+
+
+
+    // Store to localstorage
+
+
+    // Update the recent searches area
+
+
+    // Use the entry to call the openweather API
+
+
+    // Process the results from the API
+
+
+    // Display the (format) results in the output div
+
+    
+}
