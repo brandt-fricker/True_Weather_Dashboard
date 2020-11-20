a14487b5625cfb66f8af72df99e9b437510cf3fa
brandt fricker <brandtfricker@brandts-MacBook-Pro.local>
2020-11-19 17:35:11 -0700

    got current weather up and running

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var apikey = "2d3fc2e3e0711ca594c4d9d5c7d37e7f"
-// var citynameTest = "Atlanta"
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citynameTest + "&units=imperial&appid=2d3fc2e3e0711ca594c4d9d5c7d37e7f";
+// var apikey = "2d3fc2e3e0711ca594c4d9d5c7d37e7f"
+
 
 
 function userCity(citySearch) {
     $(".5days-container").empty()
     // let citySearch = document.querySelector("#city").value;
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial&appid=2d3fc2e3e0711ca594c4d9d5c7d37e7f";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=2d3fc2e3e0711ca594c4d9d5c7d37e7f";
     // Get the entry from the input field.
     // let citySearch = document.querySelector("#city").value;
     
@@ -35,6 +34,7 @@ function userCity(citySearch) {
             var theWindSpeed = burrito.wind.speed
             var latt=burrito.coord.lat
             var long=burrito.coord.lon 
+            var weatherIcon = burrito.weather[0].icon
             console.log(cityTheUserSelected)
             var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?lat="+latt+"&lon="+long+"&appid=2d3fc2e3e0711ca594c4d9d5c7d37e7f"
             $.ajax({
@@ -42,7 +42,28 @@ function userCity(citySearch) {
                 url: uvIndex,
             }).then(function(uv){
                 console.log(uv);
+                var cityUv = uv.value;
+                var d  = new Date();
+                //var d = $.format.date(new Date(), 'dd/MM/yyyy');
+                var dd = d.getDate(); 
+                var mm = d.getMonth() + 1;
+                var yyyy = d.getFullYear(); 
+                var newHtml = `
+                    <h3 class="cardBody" id="cityName">${cityTheUserSelected} (${mm}/${dd}/${yyyy})</h3>
+                    <img src=" http://openweathermap.org/img/wn/${weatherIcon}@2x.png">
+                    <p class="cardBody">Temperature: ${theTemp}</p>
+                    <p class="cardBody">Humidity: ${theHumidity}</p>
+                    <p class="cardBody">Wind Speed: ${theWindSpeed} MPH</p>
+                    <p class="cardBody">UV Index: ${cityUv}</p>
+                    `;
+                    
+                // Insert HTML into div
+                $("#currentWeather").html(newHtml);
+
             })
+          
+
+
         });
         
         
