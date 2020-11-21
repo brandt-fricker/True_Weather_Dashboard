54716dbefe802d58e71671d77705ca0f9705fa82
brandt fricker <brandtfricker@brandts-MacBook-Pro.local>
2020-11-21 13:23:52 -0700

    js done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  var apikey = "2d3fc2e3e0711ca594c4d9d5c7d37e7f"
-
+var searchList = JSON.parse(localStorage.getItem("userCitiesSearched")) || [];
 
 
 function userCity(citySearch) {
@@ -8,12 +8,6 @@ function userCity(citySearch) {
 
     if (citySearch !== "" || citySearch !== undefined) {
         citySearch.trim();
-        localStorage.setItem("UserCitySelection", JSON.stringify(citySearch));
-        var ul = document.getElementById("myUL");
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(citySearch));
-        li.classList.add("searchTerm")
-        ul.appendChild(li);
         $.ajax({
             method: "GET",
             url: queryURL,
@@ -38,14 +32,14 @@ function userCity(citySearch) {
                 var dd = d.getDate(); 
                 var mm = d.getMonth() + 1;
                 var yyyy = d.getFullYear(); 
-                var newHtml = `
+                var newHtml = $(`
                 <h3 class="cardBody" id="cityName">${cityTheUserSelected} (${mm}/${dd}/${yyyy})</h3>
                 <img src=" http://openweathermap.org/img/wn/${weatherIcon}@2x.png">
                 <p class="cardBody">Temperature: ${theTemp.toFixed(1)} \u00B0 F</p>
                 <p class="cardBody">Humidity: ${theHumidity}%</p>
                 <p class="cardBody">Wind Speed: ${theWindSpeed} MPH</p>
                 <p class="cardBody">UV Index: ${cityUv}</p>
-                `;
+                `);
                 
                 // Insert HTML into div
                 $("#currentWeather").html(newHtml);
@@ -59,55 +53,71 @@ function userCity(citySearch) {
             }).then(function(forecast){
                 var listOfDays = forecast.list
                 
-                var arrHolder = [];
                 
-                     for (let i = 5; i < listOfDays.length; i+=8) {
-                        var dayAtNoon =  listOfDays[i].dt_txt.split(' ')
-                        dayAtNoon=dayAtNoon[0].split("-");
-                        dayAtNoon="("+dayAtNoon[1]+"/"+dayAtNoon[2]+"/"+dayAtNoon[0]+")";
-                        
-                       var newTheTemp = listOfDays[i].main.temp.toFixed(1);
-
-                       var newTheHumidity = listOfDays[i].main.humidity;
-                      var newWeatherIcon =listOfDays[i].weather[0].icon
-                        
-
-                       
-
-                 }
-                    var new5day = `<h3 class="cardBody" id="cityName">${dayAtNoon}</h3>
-                     <img src=" http://openweathermap.org/img/wn/${newWeatherIcon}@2x.png">
-                     <p class="cardBody">Temp: ${newTheTemp} \u00B0F</p>
-                     <p class="cardBody">Humidity: ${newTheHumidity}</p>`
+                
+                for (let i = 5; i < listOfDays.length; i+=8) {
+                    var dayAtNoon =  listOfDays[i].dt_txt.split(' ')
+                    dayAtNoon=dayAtNoon[0].split("-");
+                    dayAtNoon="("+dayAtNoon[1]+"/"+dayAtNoon[2]+"/"+dayAtNoon[0]+")";
                     
-                     $("#5days").append(new5day)
-               
-
+                    var newTheTemp = listOfDays[i].main.temp.toFixed(1);
+                    
+                    var newTheHumidity = listOfDays[i].main.humidity;
+                    var newWeatherIcon =listOfDays[i].weather[0].icon
                     
                     
-
-    
-    
-                });
-            })
-            
-        }} 
+                    var new5day = $(`<h3 class="cardBody" id="cityName">${dayAtNoon}</h3>
+                    <img src=" http://openweathermap.org/img/wn/${newWeatherIcon}@2x.png">
+                    <p class="cardBody">Temp: ${newTheTemp} \u00B0F</p>
+                    <p class="cardBody">Humidity: ${newTheHumidity}</p>`)
+                    
+                    $("#5days").append(new5day)
+                    
+                    
+                }
+                
+                
+                
+                
+                
+                
+                
+            });
+        })
         
-            
-        
-
-
-
-$('#searchBtn').on('click', function(){
-    var city = $('#city').val();
-    userCity(city);
-});
-$(document).on("click", ".searchTerm", function(event){
+    }} 
     
-    var city = event.target.textContent;
-    userCity(city);
-
-})
-
+    
+    
+    
+    
+    
+    $('#searchBtn').on('click', function(){
+        var city = $('#city').val();
+        userCity(city);
+        searchList.push(city);
+        localStorage.setItem("userCitiesSearched", JSON.stringify(searchList));
+        var ul = $("#myUL");
+        var li = $(`<li class="searchTerm"> ${city} </li>`);
+    
+        ul.append(li);
+    });
+    $(document).on("click", ".searchTerm", function(event){
+        
+        var city = event.target.textContent;
+        userCity(city);
+        
+    })
+function pastSearchList(){
+    for (let i = 0; i < searchList.length; i++) {
+        const city = searchList[i];
+        var ul = $("#myUL");
+        var li = $(`<li class="searchTerm"> ${city} </li>`);
+    
+        ul.append(li);
+        
+    }
+}
+pastSearchList();
 
 
