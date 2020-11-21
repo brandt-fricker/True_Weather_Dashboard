989f0ab2b3082a212af088d74089d64ad8b51cdd
brandt fricker <brandtfricker@brandts-MacBook-Pro.local>
2020-11-21 12:25:32 -0700

    hopefully almost there

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function userCity(citySearch) {
             method: "GET",
             url: queryURL,
         }).then(function (burrito) {
-            console.log(burrito)
+            
             var cityTheUserSelected = burrito.name;
             var theTemp = burrito.main.temp;
             var theHumidity = burrito.main.humidity
@@ -26,13 +26,13 @@ function userCity(citySearch) {
             var latt=burrito.coord.lat
             var long=burrito.coord.lon 
             var weatherIcon = burrito.weather[0].icon
-            console.log(cityTheUserSelected)
+            
             var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?lat="+latt+"&lon="+long+"&appid="+apikey;
             $.ajax({
                 method:"GET",
                 url: uvIndex,
             }).then(function(uv){
-                console.log(uv);
+                
                 var cityUv = uv.value;
                 var d  = new Date();
                 var dd = d.getDate(); 
@@ -58,27 +58,34 @@ function userCity(citySearch) {
                 url: queryURLForecast,
             }).then(function(forecast){
                 var listOfDays = forecast.list
-                console.log(listOfDays[3])
+                
+                var arrHolder = [];
                 
                      for (let i = 5; i < listOfDays.length; i+=8) {
                         var dayAtNoon =  listOfDays[i].dt_txt.split(' ')
                         dayAtNoon=dayAtNoon[0].split("-");
                         dayAtNoon="("+dayAtNoon[1]+"/"+dayAtNoon[2]+"/"+dayAtNoon[0]+")";
                         
-                       var newTheTemp = listOfDays[i].main.temp;
+                       var newTheTemp = listOfDays[i].main.temp.toFixed(1);
+
                        var newTheHumidity = listOfDays[i].main.humidity;
                       var newWeatherIcon =listOfDays[i].weather[0].icon
-                        console.log(dayAtNoon)
-                 }
-                         
-                        
                         
 
-                    new5day = `<h3 class="cardBody" id="cityName">${dayAtNoon}</h3>
-                    <img src=" http://openweathermap.org/img/wn/${newWeatherIcon}@2x.png">
-                    <p class="cardBody">Temp: ${newTheTemp.toFixed(1)} \u00B0F</p>
-                    <p class="cardBody">Humidity: ${newTheHumidity}%</p>`
-                    $("#5days").html(new5day)
+                       
+
+                 }
+                    var new5day = `<h3 class="cardBody" id="cityName">${dayAtNoon}</h3>
+                     <img src=" http://openweathermap.org/img/wn/${newWeatherIcon}@2x.png">
+                     <p class="cardBody">Temp: ${newTheTemp} \u00B0F</p>
+                     <p class="cardBody">Humidity: ${newTheHumidity}</p>`
+                    
+                     $("#5days").append(new5day)
+               
+
+                    
+                    
+
     
     
                 });
@@ -96,7 +103,7 @@ $('#searchBtn').on('click', function(){
     userCity(city);
 });
 $(document).on("click", ".searchTerm", function(event){
-    console.log(event);
+    
     var city = event.target.textContent;
     userCity(city);
 
